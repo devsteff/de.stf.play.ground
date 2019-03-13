0cfd2f863a569516686c4b4001e2b2e5a51558c4
Stefan Flick <stefan.flick@compeple.de>
2019-03-13 01:18:49 +0100

    add PDF creation with iText Tests

--- src/test/kotlin/de/stf/play/ground/test/OpenPDFTest.kt
+++ src/test/kotlin/de/stf/play/ground/test/OpenPDFTest.kt
@@ -4,56 +4,28 @@ import com.lowagie.text.Document
 import com.lowagie.text.DocumentException
 import com.lowagie.text.Element
 import com.lowagie.text.Paragraph
+import com.lowagie.text.Phrase
+import com.lowagie.text.alignment.HorizontalAlignment
+import com.lowagie.text.pdf.PdfPCell
+import com.lowagie.text.pdf.PdfPTable
 import com.lowagie.text.pdf.PdfWriter
-import de.stf.play.ground.test.OpenPDFTest.Companion.BASEDIR
-import de.stf.play.ground.test.OpenPDFTest.Companion.success
 import org.junit.After
 import org.junit.AfterClass
 import org.junit.Assert
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
+import java.awt.Color
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
 class OpenPDFTest {
-    companion object {
-        val BASEDIR = File("./output/test/pdf")
-        var success = true
-
-        @JvmStatic
-        @BeforeClass
-        fun setupBeforeClass() {
-            println("*** setup Class")
-            if (BASEDIR.exists()) {
-                BASEDIR.deleteRecursively()
-                println("*** cleanup $BASEDIR")
-            }
-            println("*** create basedir for PDF output at $BASEDIR")
-            BASEDIR.mkdirs()
-        }
-
-        @JvmStatic
-        @AfterClass
-        fun teardownAfterClass() {
-            println("*** teardown Class")
-        }
-    }
-
-    @Before
-    fun setup() {
-        println("*** setup")
-    }
-
-    @After
-    fun teardown() {
-        println("*** teardown")
-    }
-
     @Test
-    fun helloWorld() {
-        val pdfFile = File("$BASEDIR/HelloWorld.pdf")
+    //@DisplayName("This is my test")
+    //@Tag("It is my tag")
+    fun helloWorld() { // TestInfo testInfo) {
+        val pdfFile = File("$BASEDIR/helloWorld.pdf")
         // step 1: creation of a document-object
         val document = Document()
         try {
@@ -77,15 +49,14 @@ class OpenPDFTest {
 
     @Test
     fun simpleLine() {
-        val result = executeTest("simpleLine") { document ->
+        executeTest("simpleLine") { document ->
             document.add(Paragraph("Hello World"))
         }
-        result.hashCode()
     }
 
     @Test
     fun simpleText() {
-        val result = executeTest("simpleText") { document ->
+        executeTest("simpleText") { document ->
             for (i: Int in 1..125) {
                 document.add(Paragraph("Hello World $i"))
             }
@@ -94,15 +65,21 @@ class OpenPDFTest {
 
     @Test
     fun marginTest() {
-        val result = executeTest("marginTest") { document ->
+        executeTest("marginTest") { document ->
             with(document) {
-                add(Paragraph("The left margin of this document is 36pt (0.5 inch); the right margin 72pt " +
-                    "(1 inch); the top margin 108pt (1.5 inch); the bottom margin 180pt (2.5 inch). "))
+                add(
+                    Paragraph(
+                        "The left margin of this document is 36pt (0.5 inch); the right margin 72pt " +
+                            "(1 inch); the top margin 108pt (1.5 inch); the bottom margin 180pt (2.5 inch). "
+                    )
+                )
                 val paragraph = Paragraph()
                 paragraph.setAlignment(Element.ALIGN_JUSTIFIED)
                 for (i in 0..19) {
-                    paragraph.add("Hello World, Hello Sun, Hello Moon, Hello Stars, Hello Sea, Hello Land, " +
-                        "Hello People. ")
+                    paragraph.add(
+                        "Hello World, Hello Sun, Hello Moon, Hello Stars, Hello Sea, Hello Land, " +
+                            "Hello People. "
+                    )
                 }
                 add(paragraph)
                 setMargins(180F, 108F, 72F, 36F)
@@ -115,6 +92,57 @@ class OpenPDFTest {
         }
     }
 
+    @Test
+    fun simpleTable() {
+        executeTest("simpleTable") { document ->
+            val table = PdfPTable(3)
+            var cell = PdfPCell(Phrase("Cell with colspan 3"))
+            with(cell) {
+                colspan = 3
+                backgroundColor = Color.GRAY
+                defaultPadding()
+                horizontalAlignment = HorizontalAlignment.CENTER.id
+            }
+            table.addCell(cell)
+            cell = PdfPCell(Phrase("Cell with rowspan 2")).defaultPadding()
+            cell.rowspan = 2
+            cell.backgroundColor = Color.LIGHT_GRAY
+            table.addCell(cell)
+            document.add(
+                table.addTextCell("R1, C1")
+                    .addTextCell("R1, C2", HorizontalAlignment.RIGHT.id)
+                    .addTextCell("R2,\nC1")
+                    .addTextCell("R2, C2", HorizontalAlignment.CENTER.id)
+            )
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------
+    // Helper functions
+    // -----------------------------------------------------------------------------------------------
+    private fun PdfPTable.addTextCell(text: String,
+        hAlign: Int = HorizontalAlignment.LEFT.id): PdfPTable {
+        addCell(createCell(text, hAlign))
+        return this
+    }
+
+    private fun createCell(text: String, hAlign: Int = HorizontalAlignment.LEFT.id): PdfPCell {
+        val cell = PdfPCell(Phrase(text)).defaultPadding()
+        cell.horizontalAlignment = hAlign
+        return cell
+    }
+
+    private fun PdfPCell.defaultPadding(): PdfPCell {
+        paddingTop = 1f
+        paddingBottom = 5f
+        paddingLeft = 5f
+        paddingRight = 5f
+        return this
+    }
+
+    // -----------------------------------------------------------------------------------------------
+    // Testcase executor
+    // -----------------------------------------------------------------------------------------------
     private fun executeTest(testName: String, pdfCreator: (document: Document) -> Unit) {
         val document = Document() // step 1: creation of a document-object
         val pdfFile = File("$BASEDIR/$testName.pdf")
@@ -134,14 +162,57 @@ class OpenPDFTest {
                 addAuthor("st@deutschefin.tech")
             }
             pdfCreator(document)
-        } catch (ex: Exception ) {
+        } catch (ex: Exception) {
             println("error executing test '$testName': ${ex::class.java.name} ${ex.message}")
         }
         // step 5: we close the document
         document.close()
-        val successfull = pdfFile.exists() && pdfFile.isFile
-        if(success) success = successfull
-        Assert.assertTrue("### PDF file '$pdfFile' not created", successfull)
-        Assert.assertTrue("### PDF file '$pdfFile' content not created", pdfFile.length()>890)
+        val successful = pdfFile.exists() && pdfFile.isFile
+        if (SUCCESS) SUCCESS = successful
+        Assert.assertTrue("### PDF file '$pdfFile' not created", successful)
+        Assert.assertTrue("### PDF file '$pdfFile' content not created",
+            pdfFile.length() > 890)
+    }
+
+    // -----------------------------------------------------------------------------------------------
+    // JUnit cycle functions for each testcase
+    // -----------------------------------------------------------------------------------------------
+    @Before
+    fun setup() {
+        println("*** setup test")
+        // Thread.currentThread().stackTrace.iterator().forEach {
+        //    println("    ${it.className}.${it.methodName} (line ${it.lineNumber})")
+        // }
+    }
+
+    @After
+    fun teardown() {
+        println("*** teardown test")
+    }
+
+    // -----------------------------------------------------------------------------------------------
+    // JUnit cycle functions for each test class
+    // -----------------------------------------------------------------------------------------------
+    companion object {
+        val BASEDIR = File("./output/test/pdf")
+        var SUCCESS = true
+
+        @JvmStatic
+        @BeforeClass
+        fun setupBeforeClass() {
+            println("*** class setup $javaClass")
+            if (BASEDIR.exists()) {
+                BASEDIR.deleteRecursively()
+                println("*** class cleanup $BASEDIR")
+            }
+            println("*** class create basedir for PDF output at $BASEDIR")
+            BASEDIR.mkdirs()
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun teardownAfterClass() {
+            println("*** class teardown $javaClass")
+        }
     }
 }
--- src/test/kotlin/de/stf/play/ground/test/ReactiveTest.kt
+++ src/test/kotlin/de/stf/play/ground/test/ReactiveTest.kt
@@ -1,15 +1,19 @@
 package de.stf.play.ground.test
 
+import mu.KotlinLogging
 import org.junit.Test
 import reactor.core.publisher.Flux
 import reactor.test.StepVerifier
 import java.util.Arrays
 import java.util.function.BiFunction
 
+private val logger = KotlinLogging.logger {}
+
 class ReactiveTest {
     @Test
     fun fewWords() {
         val fewWords = Flux.just("Hello", "World")
+        logger.debug { "############ fewWords" }
         StepVerifier.create(fewWords)
             .expectNext("Hello")
             .expectNextCount(1)
@@ -21,6 +25,7 @@ class ReactiveTest {
     fun manyWordsSortedTest() {
         val manyWordsSorted = WORDS
             .sort()
+        logger.info { "############ manyWordsSortedTest" }
         StepVerifier.create(manyWordsSorted)
             .expectNext("brown")
             .expectNextCount(7)
@@ -33,6 +38,7 @@ class ReactiveTest {
     fun manyWordsReversedSortedTest() {
         val manyWordsReversedSorted = WORDS
             .sort { o1, o2 -> o1.compareTo(o2) * -1 }
+        logger.warn { "############ manyWordsReversedSortedTest" }
         StepVerifier.create(manyWordsReversedSorted)
             .expectNext("the")
             .expectNextCount(7)
@@ -59,7 +65,8 @@ class ReactiveTest {
                 BiFunction<String, Int, String> { s, c -> String.format("%2d. %s", c, s) }
             )
             .map { it }
-            .log()
+            //.log()
+        logger.error { "############ findingMissingLetter" }
         StepVerifier.create(manyLetters)
             .expectNext(" 1. ")
             .expectNext(" 2. a")
@@ -74,9 +81,10 @@ class ReactiveTest {
     @Test
     fun givenFluxes_thenZipWith() {
         val fluxOfIntegers = EVEN_INT_FLUX
-            .log()
+            //.log()
             .zipWith(ODD_INT_FLUX, BiFunction<Int, Int, String> { even, odd -> "$even*$odd=${even * odd}" })
-            .log()
+            //.log()
+        logger.debug { "############ givenFluxes_thenZipWith" }
         StepVerifier.create(fluxOfIntegers)
             .expectNext("2*1=2")  // 2 * 1
             .expectNext("4*3=12") // 4 * 3
